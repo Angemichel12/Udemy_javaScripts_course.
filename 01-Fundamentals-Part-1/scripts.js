f662bdfd8be91006a62de730718039f737ef5b5b
Ange Michel <angemicheluwizeye@gmail.com>
2023-12-21 11:50:49 +0100

    start learn js on udemy

---
+++ 01-Fundamentals-Part-1/scripts.js
@@ -0,0 +1,5 @@
+let country = "Rwanda";
+let continent = "Africa";
+let population = 12;
+
+console.log("My country is: " + country + "and my continent is " + continent + "with population of " + population);
