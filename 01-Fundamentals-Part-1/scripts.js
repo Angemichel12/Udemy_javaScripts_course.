d1c27104254ea780c925370db89f61db4a103035
Ange Michel <angemicheluwizeye@gmail.com>
2023-12-22 00:14:30 +0100

    javascripts operators

--- 01-Fundamentals-Part-1/scripts.js
+++ 01-Fundamentals-Part-1/scripts.js
@@ -1,5 +1,24 @@
-let country = "Rwanda";
-let continent = "Africa";
-let population = 12;
+// let country = "Rwanda";
+// let continent = "Africa";
+// let population = 12;
 
-console.log("My country is: " + country + "and my continent is " + continent + "with population of " + population);
+// console.log("My country is: " + country + "and my continent is " + continent + "with population of " + population);
+
+let javascriptIsFun = true;
+// let age = 23;
+// let firstName = "Michel";
+console.log(typeof javascriptIsFun);
+// console.log(typeof age);
+// console.log(typeof firstName);
+javascriptIsFun = 'Yes!'
+console.log(typeof javascriptIsFun)
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+newChar = "";
+console.log(typeof newChar);
